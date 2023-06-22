d380c1a10c6cc56e49c8bf098f4814fb3ea9e7ef
10thcode <attabadukwabena@gmail.com>
2023-06-22 05:09:52 +0000

    Update div.c: fix error message

--- div.c
+++ div.c
@@ -10,7 +10,7 @@ void _div(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		exit_code = EXIT_FAILURE;
 		return;
 	}
